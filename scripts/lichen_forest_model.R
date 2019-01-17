7429eb81ea09e27acbfebbb164136f0991022b49
Julian Klein <jg.klein@protonmail.com>
2019-01-17 12:07:53 +0100

    total number of lichens per tree and number of species added

--- scripts/lichen_forest_model.R
+++ scripts/lichen_forest_model.R
@@ -15,6 +15,7 @@ library(data.table)
 library(dplyr)
 library(boot)
 library(corrgram)
+library(ggplot2)
 # library(rjags)
 # library(coda)
 
@@ -61,6 +62,9 @@ l_obs$plot <- as.factor(paste0("plot_", l_obs$plot))
 colnames(l_obs)[3] <- "circle_10m"
 levels(l_obs$circle_10m) <- c("middle", "east", "west")
 
+## Reduce trees of unknown species:
+l_obs <- l_obs[!is.na(l_obs$Tree.species),]
+
 ## Exclude cut stumps and make seperate data set:
 l_obs_cut <- l_obs[is.na(l_obs$Tree.no), c(2:3, 8:9)]
 
@@ -85,20 +89,20 @@ ltf <- merge(l_obs_thallus,
              by = c("plot", "circle_10m")) 
 
 ## Make data frame based on occupancy of all species per tree:
-l_obs_occ <- melt(l_obs, 
-                  id.vars = colnames(l_obs)[1:11],
-                  measure.vars = colnames(l_obs)[c(12:17, 19:100, 102:107)],
-                  value.name = "observed",
-                  variable.name = "species")
+l_occ <- melt(l_obs, 
+              id.vars = colnames(l_obs)[1:11],
+              measure.vars = colnames(l_obs)[c(12:17, 19:100, 102:107)],
+              value.name = "observed",
+              variable.name = "species")
 
 ## Replace NA with 0 in species list to get presence absence per tree:
-l_obs_occ$observed[is.na(l_obs_occ$observed)] <- 0
+l_occ$observed[is.na(l_occ$observed)] <- 0
 
 ## Exclude combinations of tree species, stem/branches and lichen species that 
 ## are never observed:
-l_obs_occ <- as.data.table(l_obs_occ)
-l_obs_occ[, "observable" := ifelse(sum(observed) > 0, "yes", "no"),
-          by = c("Tree.species", "Stem.S.Branches.B.T.Total", "species")]
+l_occ <- as.data.table(l_occ)
+l_occ[, "observable" := ifelse(sum(observed) > 0, "yes", "no"),
+      by = c("Tree.species", "Stem.S.Branches.B.T.Total", "species")]
 
 ## Export non-observable and let Göran check. Adjust yes/no manually at this 
 ## point and reimport data set:
@@ -108,24 +112,48 @@ l_obs_occ[, "observable" := ifelse(sum(observed) > 0, "yes", "no"),
 # read
 
 ## Exclude non-observable:
-l_obs_occ_red <- l_obs_occ[l_obs_occ$observable == "yes", ]
+l_occ <- l_occ[l_occ$observable == "yes", ]
 
-## Merge l_obs_occ_red with forest data:
-lof <- merge(l_obs_occ_red, f_plot[, c(1:3, 6:8, 11:20)], 
-             all.x = TRUE, 
-             by = "plot") 
+## Add total observed per tree:
 
-## Calculate per tree, which percentage of possible species is present on tree:
-lof[, "perc_obs" := (sum(observed)/length(observed)), 
+l_temp <- l_occ[, "observed_t" := ifelse(sum(observed) > 0, 1, 0),
+                by = c("plot", "Tree.no", "species")]
+
+l_temp$observed <- l_temp$observed_t
+l_temp$Stem.S.Branches.B.T.Total <- "T"
+l_temp <- unique(l_temp)
+
+l_occ <- rbind(l_occ, l_temp)
+
+l_occ <- l_occ[, -15]
+
+## Bin all deciduous tree species:
+levels(l_occ$Tree.species)[levels(l_occ$Tree.species) %in% 
+                             c("Qr", "Pt", "Ag", "Bp")] <- "Dc"
+
+## Merge l_occ with forest data:
+lof <- merge(l_occ, f_plot[, c(1:3, 6:8, 11:20)], all.x = TRUE, by = "plot") 
+
+## Calculate per tree, which percentage of possible species and the absolute 
+## number of species present on that tree:
+lof[, c("perc_obs", "richness") := list((sum(observed)/length(observed)),
+                                        sum(observed)), 
     by = c("plot", "Tree.no", "Stem.S.Branches.B.T.Total")]
 
 ## Reduce to unique rows for percentage occupancy:
-lpof <- unique(lof[, -12])
+lof <- unique(lof[, -12])
 
 ## 5. Explore data graphically -------------------------------------------------
 
+g1 <- ggplot(lof, aes(x = PercentBelow5m, 
+                      y = perc_obs,
+                      fill = Tree.species, 
+                      color = Tree.species))
+g2 <- geom_point()
+g3 <- stat_smooth(method = "lm", size = 2, formula = y ~ log(x))# + I(x^2))            
+g4 <- facet_grid(. ~ Stem.S.Branches.B.T.Total) 
 
-
+g1+g3+g4
 
 ## 6. Start analysing lpof ----------------------------------------------------- 
 
