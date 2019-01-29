699ac3469e0be775f0848bde7d8e63013943d4c6
Julian Klein <jg.klein@protonmail.com>
2019-01-29 10:53:41 +0100

    JAGS model for tree level richness initiated.

--- scripts/JAGS/lichen_JAGS_ltr.R
+++ scripts/JAGS/lichen_JAGS_ltr.R
@@ -1,14 +1,43 @@
-#JAGS model template
+## lichen ltr model
+##
+## First edit: 20190129
+## Last edit: 20190129
+##
+## Author: Julian Klein
 
 model{
 
-#likelihood
-for(i in 1:nobs){
-	
-	
-	
+## likelihood
+  
+## Tree level:
+for(i in id){
+  
+  richness[i] ~ dpois(lambda[i])
+  log(lambda[i]) <- alpha_plot[plot[i]] + 
+                    beta_pine[i]*tree_sp_pine + 
+                    beta_spruce[i]*tree_sp_spruce + 
+                    beta_stem_dbh[i]*stem_dbh
+  
 }
 
+## Plot level:
+for(j in unique(plot)){
+  
+  alpha_plot[j] ~ dgamma(a_plot[j], b_plot[j])
+  mu[j] <- alpha_plot_mean + block_effect[block[j]] +
+           beta_stand_dbh[j]*stand_dbh +
+           beta_cdens[j]*canopy_density +
+           beta_udens[j]*understory_density
+  
+} 
+
+## Block level:
+for (k in 1:unique(block)){
+  
+  site.eff[k] ~ dnorm(0, tau_block)
+  
+}
+  
 #priors
 
 
